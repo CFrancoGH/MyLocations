b148f4b42eddb4f5e5780f9f8a26f2d21385a8ec
Christopher Franco <email.cfranco@gmail.com>
2021-09-20 12:00:23 -0400

    wow

--- MyLocations/SceneDelegate.swift
+++ MyLocations/SceneDelegate.swift
@@ -34,34 +34,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     listenForFatalCoreDataNotifications()
   }
 
-  func sceneDidDisconnect(_ scene: UIScene) {
-    // Called as the scene is being released by the system.
-    // This occurs shortly after the scene enters the background, or when its session is discarded.
-    // Release any resources associated with this scene that can be re-created the next time the scene connects.
-    // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-  }
+  func sceneDidDisconnect(_ scene: UIScene) {}
 
-  func sceneDidBecomeActive(_ scene: UIScene) {
-    // Called when the scene has moved from an inactive state to an active state.
-    // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-  }
+  func sceneDidBecomeActive(_ scene: UIScene) {}
 
-  func sceneWillResignActive(_ scene: UIScene) {
-    // Called when the scene will move from an active state to an inactive state.
-    // This may occur due to temporary interruptions (ex. an incoming phone call).
-  }
+  func sceneWillResignActive(_ scene: UIScene) {}
 
-  func sceneWillEnterForeground(_ scene: UIScene) {
-    // Called as the scene transitions from the background to the foreground.
-    // Use this method to undo the changes made on entering the background.
-  }
+  func sceneWillEnterForeground(_ scene: UIScene) {}
 
   func sceneDidEnterBackground(_ scene: UIScene) {
-    // Called as the scene transitions from the foreground to the background.
-    // Use this method to save data, release shared resources, and store enough scene-specific state information
-    // to restore the scene back to its current state.
 
-    // Save changes in the application's managed object context when the application transitions to the background.
     saveContext()
   }
 
@@ -85,8 +67,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       do {
         try context.save()
       } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
         let nserror = error as NSError
         fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
       }
